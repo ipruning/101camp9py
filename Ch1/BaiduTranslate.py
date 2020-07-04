a292db2cc6b598a97681c9e4f4aabe3513bafb58
Alex <30626291+Spehhhhh@users.noreply.github.com>
2020-07-04 18:30:25 +0800

    add BiaduTranslate requests test

---
+++ Ch1/BaiduTranslate.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+url = 'https://fanyi.baidu.com/sug'
+kw = input('Translate: ')
+kw = {
+    'kw': kw
+}
+header = {
+    'User-Agent': 'Chrome/83.0.4103.116'
+}
+response = requests.post(url=url, data=kw, headers=header)
+response.raise_for_status()
+response.encoding = response.apparent_encoding
+content_json = response.json()
+content = json.dumps(content_json, ensure_ascii=False)
+result = json.loads(content)
+print(result["data"])
