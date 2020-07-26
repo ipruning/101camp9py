caf90f37f1b752cc84b88f27684abe336f699dfd
Alex <30626291+Spehhhhh@users.noreply.github.com>
2020-07-27 05:45:46 +0800

    add e11

--- LPTHW/e11.py
+++ LPTHW/e11.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+'''问问题
 
-print("How old are you?", end=' ')
+'''
+
+print("How old are you?", end=' ') # 每一个打印行末尾放一个 end=' ' ，是为了告诉 print 不要另起一行。
 age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weight?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
