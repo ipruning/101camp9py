a4463a731d7b319822ed596b17c06c4fe9ae4817
Alex <30626291+Spehhhhh@users.noreply.github.com>
2020-07-27 05:45:52 +0800

    add e12

--- LPTHW/e12.py
+++ LPTHW/e12.py
@@ -1,6 +1,12 @@
-age = input("How old are you?")
-print(f"You are {age} old.")
-print("How old are you?", input())
-# Why can’t I do print("How old are you?" , input())?
-# You can, but then the result of calling input() is never saved to a variable, antd it’ll work in a strange way.
-# Try this, and then try to print out what you type. See if you can debug why this isn’t working.
+# -*- coding: utf-8 -*-
+'''提示用户
+pydoc input 可查文档。
+'''
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+print("How old are you?", input()) # input() 测试
