c45320968a8feb5fb090dda8f96e7dd4e84e8e6c
Alex <30626291+Spehhhhh@users.noreply.github.com>
2020-07-28 22:00:00 +0800

    add e32

---
+++ LPTHW/e32.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+'''循环和列表
+'''
+
+the_count = [1, 2, 3, 4,5 ] # list
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3 , 'quarters']
+
+for number in the_count:
+    print(f"This is count {number}")
+
+for fruit in fruits: # 这种循环的写法蛮意外，Python 中 for 会遍历任何序列，List Tuple 迭代器
+    print(f"A fruit of type: {fruit}")
+
+for i in change:
+    print(f"I got {i}")
+
+elements = [] # 新建 List
+
+for i in range(0, 6):
+    print(f"Adding {i} to the list.")
+    elements.append(i)
+
+for i in elements:
+    print(f"Element was: {i}")
