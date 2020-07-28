0f09ab5e5f28da7ba3237500fcf2009dd0b31ea8
Alex <30626291+Spehhhhh@users.noreply.github.com>
2020-07-28 16:08:19 +0800

    add e30

---
+++ LPTHW/e30.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''Else 和 if
+'''
+
+people = 30
+cars = 40
+
+if cars > people:
+    print("We should take the cars.")
+elif cars < people:
+    print("We should not take the cars.")
+else:
+    print("We can't decide.")
