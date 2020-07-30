cc639b394280b335e9d1fe098966720aef4a1df5
Alex <30626291+Spehhhhh@users.noreply.github.com>
2020-07-31 01:27:15 +0800

    add 单元测试

--- PoL/tests/test_main.py
+++ PoL/tests/test_main.py
@@ -1,14 +1,29 @@
 from nose.tools import *
-import pol
+from pol import api
 
 
-def setup():
-    print("SETUP!")
+# nosetests
 
 
-def teardown():
-    print("TEAR DOWN!")
+def test_QueryGraphQL():
+    query = """
+    {
+    project(fullPath: "101camp/2py/tasks") {
+        id
+    }
+    }
+    """
+    issue = api.QueryGraphQL("101camp9py-id", query, {})
+    assert_equal(issue.run_query()['data']['project']['id'], "gid://gitlab/Project/13020942"")
 
 
-def test_basic():
-    print("I RAN!")
+def test_QueryREST():
+    branch = api.QueryREST(
+        "101camp9py-branch", "https://gitlab.com/api/v4/projects/18907382/repository/branches")
+    branch.save_query()
+    branch_list = []
+    for item in branch.run_query():
+        name = item['name']
+        if name != 'master':
+            branch_list.append(name)
+    assert_equal(len(branch_list), 20)
